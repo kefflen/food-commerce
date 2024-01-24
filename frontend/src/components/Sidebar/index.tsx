c065abb11788ca29736aa8de1b174b0669d40edd
kefflen <kefflenjava@hotmail.com>
2024-01-24 13:42:26 -0300

    importing svg as react component

--- frontend/src/components/Sidebar/index.tsx
+++ frontend/src/components/Sidebar/index.tsx
@@ -1,8 +1,9 @@
-import bugerAndSoda from '../../assets/burger-and-soda.svg'
-import burger from '../../assets/burger.svg'
-import menu from '../../assets/menu.svg'
-import pizza from '../../assets/pizza.svg'
-import soda from '../../assets/soda.svg'
+
+import BugerAndSoda from '../../assets/burger-and-soda.svg?react'
+import Burger from '../../assets/burger.svg?react'
+import MenuIcon from '../../assets/menu.svg?react'
+import Pizza from '../../assets/pizza.svg?react'
+import Soda from '../../assets/soda.svg?react'
 import { Container, Menu, MenuItem, Nav } from "./styles"
 
 //TODO: Refatorar para que tenha um componente MenuItem que recebe a imagem e o texto como props
@@ -10,31 +11,31 @@ export function Sidebar() {
   return (
     <Container>
       <button type="button">
-        <img src={menu}/>
+        <MenuIcon />
       </button>
       <Nav>
         <Menu>
           <MenuItem>
             <a href="#">
-              <img src={burger} alt="Hambugueres" />
+              <Burger/>
               <span>Hambugueres</span>
             </a>
           </MenuItem>
           <MenuItem>
             <a href="#">
-              <img src={bugerAndSoda} alt="Combos" />
+              <BugerAndSoda />
               <span>Combos</span>
             </a>
           </MenuItem>
           <MenuItem>
             <a href="#">
-              <img src={pizza} alt="Pizzas" />
+              <Pizza />
               <span>Pizzas</span>
             </a>
           </MenuItem>
           <MenuItem>
             <a href="#">
-              <img src={soda} alt="Refrigerantes" />
+              <Soda />
               <span>Refrigerantes</span>
             </a>
           </MenuItem>
